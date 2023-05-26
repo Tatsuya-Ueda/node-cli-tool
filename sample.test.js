b24bb825832cfb8ace4ae0cb7b8c9dde3f93bcc0
Tatsuya-Ueda <74095322+Tatsuya-Ueda@users.noreply.github.com>
2023-05-26 22:23:56 +0900

    テストランナーのパッケージを導入，テストを少し記述

---
+++ sample.test.js
@@ -0,0 +1,20 @@
+test("sample test", () => {
+  expect(1 + 2).toStrictEqual(3);
+});
+
+// const assert = require("assert");
+// assert.strictEqual(1 + 2, 3, "1+2 strictly equal to 3!");
+
+// const obj1 = {
+//   a: {
+//     b: 1,
+//   },
+// };
+
+// const obj2 = {
+//   a: {
+//     b: 2,
+//   },
+// };
+
+// assert.deepStrictEqual(obj1, obj2, "obj1 equals to obj2!");
