677245e6dab114cceeec66693047c9a16b9919c6
Tatsuya-Ueda <74095322+Tatsuya-Ueda@users.noreply.github.com>
2023-05-26 20:54:54 +0900

    HTMLの書き出し機能を実装

--- index.js
+++ index.js
@@ -1,27 +1,34 @@
 const path = require("path");
+const { marked } = require("marked");
 const yargs = require("yargs/yargs");
 const { hideBin } = require("yargs/helpers");
 const { getPackageName } = require("./lib/name");
-const { readMarkdownFileSync } = require("./lib/file");
+const { readMarkdownFileSync, writeHtmlFileSync } = require("./lib/file");
 
-// コマンドライン引数を処理
+// コマンドライン引数のオプションの設定を行う
 const { argv } = yargs(hideBin(process.argv))
   .option("name", {
     describe: "CLI名を表示します",
   })
   .option("file", {
     describe: ".mdのパスを指定します",
+  })
+  .option("out", {
+    describe: "出力するHTMLファイルの名前を指定します",
+    default: "article.html",
   });
-console.log(argv);
-console.log(argv.file);
-console.log();
 
-// コマンドライン引数に応じた処理
+// 以下，メイン処理
+
+// --name で実行された場合
 if (argv.name) {
   const name = getPackageName();
   console.log(name);
   process.exit(0);
 }
 
+// --file=filename で実行された場合
 const markdownStr = readMarkdownFileSync(path.resolve(__dirname, argv.file));
-console.log(markdownStr);
+const html = marked(markdownStr);
+// console.log(markdownStr);
+writeHtmlFileSync(path.resolve(__dirname, argv.out), html);
