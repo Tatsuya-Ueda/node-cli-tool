94c8ef4f14eeab9b705fc2c19bffcc406a53a6bf
Tatsuya-Ueda <74095322+Tatsuya-Ueda@users.noreply.github.com>
2023-05-26 21:32:37 +0900

    eslintの導入と設定ファイルの作成

---
+++ .eslintrc.js
@@ -0,0 +1,14 @@
+module.exports = {
+  env: {
+    commonjs: true,
+    es2021: true,
+    node: true,
+  },
+  parserOptions: {
+    ecmaVersion: 12,
+  },
+  rules: {
+    quotes: ["error", "double"],
+  },
+  //   extends: "eslint:recommended",
+};
